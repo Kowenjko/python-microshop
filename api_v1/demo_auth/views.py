3669335d3d9bbff5a7f8716e0e4075b5e2c963e8
Kowenjko <ortoswt@gmail.com>
2025-11-06 17:26:15 +0200

    add auth for cookie

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -21,6 +21,9 @@ static_auth_token_to_username = {
     "a14f178e75dee69fa66ff3fad9db0daa": "john",
 }
 
+COOKIES: dict[str, dict[str, Any]] = {}
+COOKIE_SESSION_ID_KEY = "web-app-session-id"
+
 
 @router.get("/basic-auth/")
 async def demo_basic_auth_credentials(
@@ -62,4 +65,82 @@ def demo_basic_auth_username(
     return {
         "message": f"Hi, {auth_username}!",
         "username": auth_username,
+    }
+
+
+def get_username_by_static_auth_token(
+    static_token: str = Header(alias="x-auth-token"),
+) -> str:
+    if username := static_auth_token_to_username.get(static_token):
+        return username
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="token invalid",
+    )
+
+
+@router.get("/some-http-header-auth/")
+def demo_auth_some_http_header(
+    username: str = Depends(get_username_by_static_auth_token),
+):
+    return {
+        "message": f"Hi, {username}!",
+        "username": username,
+    }
+
+
+def generate_session_id() -> str:
+    return uuid.uuid4().hex
+
+
+def get_session_data(
+    session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
+) -> dict:
+    if session_id not in COOKIES:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="not authenticated",
+        )
+
+    return COOKIES[session_id]
+
+
+@router.post("/login-cookie/")
+def demo_auth_login_set_cookie(
+    response: Response,
+    # auth_username: str = Depends(get_auth_user_username),
+    username: str = Depends(get_username_by_static_auth_token),
+):
+    session_id = generate_session_id()
+    COOKIES[session_id] = {
+        "username": username,
+        "login_at": int(time()),
+    }
+    response.set_cookie(COOKIE_SESSION_ID_KEY, session_id)
+    return {"result": "ok"}
+
+
+@router.get("/check-cookie/")
+def demo_auth_check_cookie(
+    user_session_data: dict = Depends(get_session_data),
+):
+    username = user_session_data["username"]
+    return {
+        "message": f"Hello, {username}!",
+        **user_session_data,
+    }
+
+
+@router.get("/logout-cookie/")
+def demo_auth_logout_cookie(
+    response: Response,
+    session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
+    user_session_data: dict = Depends(get_session_data),
+):
+    COOKIES.pop(session_id)
+    response.delete_cookie(COOKIE_SESSION_ID_KEY)
+    username = user_session_data["username"]
+    return {
+        "message": f"Bye, {username}!",
     }
