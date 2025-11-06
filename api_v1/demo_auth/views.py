262b9cc09a1e8d784d656e7871bc7640251ca19c
Kowenjko <ortoswt@gmail.com>
2025-11-06 17:01:51 +0200

    add basic-auth-username

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,10 +1,25 @@
-from typing import Annotated
-from fastapi import APIRouter, Depends
+import secrets
+import uuid
+from typing import Annotated, Any
+from time import time
+
+from fastapi import APIRouter, Depends, HTTPException, status, Header, Response, Cookie
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 
 router = APIRouter(prefix="/demo-auth", tags=["Demo Auth"])
 security = HTTPBasic()
+
+usernames_to_passwords = {
+    "admin": "admin",
+    "john": "password",
+}
+
+
+static_auth_token_to_username = {
+    "a0787852e766b02e87f6dd15e4c3d1f1": "admin",
+    "a14f178e75dee69fa66ff3fad9db0daa": "john",
+}
 
 
 @router.get("/basic-auth/")
@@ -15,4 +30,36 @@ async def demo_basic_auth_credentials(
         "message": "Hi!",
         "username": credentials.username,
         "password": credentials.password,
+    }
+
+
+def get_auth_user_username(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+) -> str:
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
+    correct_password = usernames_to_passwords.get(credentials.username)
+    if correct_password is None:
+        raise unauthed_exc
+
+    # secrets
+    if not secrets.compare_digest(
+        credentials.password.encode("utf-8"),
+        correct_password.encode("utf-8"),
+    ):
+        raise unauthed_exc
+
+    return credentials.username
+
+
+@router.get("/basic-auth-username/")
+def demo_basic_auth_username(
+    auth_username: str = Depends(get_auth_user_username),
+):
+    return {
+        "message": f"Hi, {auth_username}!",
+        "username": auth_username,
     }
