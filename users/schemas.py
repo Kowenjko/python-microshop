427d8225f459b43f2efa5fb0ea709c240648c592
Kowenjko <ortoswt@gmail.com>
2025-11-07 11:00:00 +0200

    add valid jwt token

--- users/schemas.py
+++ users/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import EmailStr, BaseModel, Field
+from pydantic import EmailStr, BaseModel, Field, ConfigDict
 from annotated_types import MinLen, MaxLen
 from typing import Annotated
 
@@ -7,3 +7,12 @@ class CreateUser(BaseModel):
     email: EmailStr
     first_name: str = Field(..., min_length=3, max_length=20)
     last_name: Annotated[str, MinLen(3), MaxLen(20)]
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool = True
